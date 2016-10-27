3b5e4734e0c10585938d5bc5f66a847b9a04ef5d
Kye Hohenberger <kye@sideway.com>
2016-10-27 12:49:00 -0500

    Testing an error with preact children always being different

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@
 // https://github.com/developit/preact-compat/blob/7c5de00e7c85e2ffd011bf3af02899b63f699d3a/src/index.js#L349
 function shallowDiffers (a, b) {
   for (let i in a) if (!(i in b)) return true
-  for (let i in b) if (a[i] !== b[i]) return true
+  for (let i in b) {
+    if (a[i] !== b[i]) {
+      console.log(i + ' is not equal', 'value a: ', a[i], 'value b: ', b[i])
+      return true
+    }
+  }
   return false
 }
 
 export default (instance, nextProps, nextState) => {
-  return (
-    shallowDiffers(instance.props, nextProps) ||
-    shallowDiffers(instance.state, nextState)
-  )
+  let propsDiffers = shallowDiffers(instance.props, nextProps)
+  console.log('propsDiffers', propsDiffers)
+  const stateDiffers = shallowDiffers(instance.state, nextState)
+  console.log('stateDiffers', stateDiffers)
+  return (propsDiffers || stateDiffers)
 }
--- test/index.js
+++ test/index.js
@@ -4,15 +4,14 @@ import { h, render, rerender, Component } from 'preact'
 import shallowCompare from '../src/index'
 import undom from 'undom'
 
-const document = undom()
-global.document = document
+Object.assign(global, undom())
 
-describe('Preact Shallow Compare', () => {
+describe('Shallow Compare', () => {
   let scratch
 
   before(() => {
     scratch = document.createElement('div')
-    (document.body || document.documentElement).appendChild(scratch)
+    document.documentElement.appendChild(scratch)
   })
 
   beforeEach(() => {
@@ -24,7 +23,7 @@ describe('Preact Shallow Compare', () => {
     scratch = null
   })
 
-  it('only renders when props or state changes', () => {
+  it('only renders state changes', () => {
     let renderCalls = 0
     let setState
 
@@ -45,11 +44,58 @@ describe('Preact Shallow Compare', () => {
 
       render () {
         ++renderCalls
-        return h('div', null, this.props.fieldA)
+        return h('div', null, 'test')
       }
     }
 
-    render(h(ShallowTestComponent, { fieldA: 1 }), scratch)
+    render(h(ShallowTestComponent, {}), scratch)
+    expect(renderCalls).toBe(1)
+    rerender()
+    expect(renderCalls).toBe(1)
+    setState({ color: 'blue' })
+    rerender()
+    expect(renderCalls).toBe(2)
+    setState({ color: 'blue' })
+    rerender()
+    expect(renderCalls).toBe(2)
+    setState({ size: 'large' })
+    rerender()
+    expect(renderCalls).toBe(3)
+  })
+
+  it.only('only renders props changes', () => {
+    let renderCalls = 0
+    let setState
+
+    class ShallowTestComponent extends Component {
+      shouldComponentUpdate (nextProps, nextState) {
+        console.log()
+        console.log('prevProps', JSON.stringify(this.props))
+        console.log('nextProps', JSON.stringify(nextProps))
+        console.log('shouldComponentUpdate', shallowCompare(this, nextProps, nextState))
+        return shallowCompare(this, nextProps, nextState)
+      }
+
+      render () {
+        ++renderCalls
+        console.log('render', this.props.color)
+        return h('div', {}, this.props.color, this.props.children)
+      }
+    }
+
+    class Container extends Component {
+      constructor (props) {
+        super(props)
+        this.state = { color: 'red' }
+        setState = this.setState.bind(this)
+      }
+
+      render () {
+        return h(ShallowTestComponent, { color: this.state.color }, 'test')
+      }
+    }
+
+    render(h(Container, {}), scratch)
     expect(renderCalls).toBe(1)
     rerender()
     expect(renderCalls).toBe(1)
